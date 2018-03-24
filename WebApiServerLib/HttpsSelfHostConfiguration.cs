4fa5b75c78971bbfa56e4b5d3d012dbb1f6e0a65
Charles Piehl <charlespiehl@gmail.com>
2018-03-24 10:15:24 -0700

    Add project files.

---
+++ WebApiServerLib/HttpsSelfHostConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ServiceModel.Channels;
+using System.Web.Http.SelfHost;
+using System.Web.Http.SelfHost.Channels;
+
+namespace WebApiServerLib
+{
+	class HttpsSelfHostConfiguration : HttpSelfHostConfiguration
+	{
+		public HttpsSelfHostConfiguration(Uri baseAddress) : base(baseAddress) { }
+
+        protected override BindingParameterCollection OnConfigureBinding(HttpBinding httpBinding)
+        {
+            if (BaseAddress.ToString().ToLower().Contains("https://"))
+            {
+                httpBinding.Security.Mode = HttpBindingSecurityMode.Transport;
+            }
+
+            return base.OnConfigureBinding(httpBinding);
+        }
+	}
+}
---
+++ WebApiServerLib/WebApiServer.cs
@@ -0,0 +1,71 @@
+﻿using CPLogging;
+using System;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace WebApiServerLib
+{
+	public class WebApiServer
+	{
+		HttpSelfHostServer server;
+		HttpSelfHostConfiguration config;
+		Uri hostUri;
+		public int MaxBufferSize = int.MaxValue;
+		public int MaxReceivedMessageSize = int.MaxValue;
+
+		public WebApiServer(string hostAddr)
+		{
+			this.hostUri = new Uri(hostAddr);
+		}
+
+		public bool Start()
+		{
+			try
+			{
+				config = new HttpSelfHostConfiguration(this.hostUri);
+				config.Routes.MapHttpRoute("API DEFAULT", "{controller}/{action}",
+					new { id = RouteParameter.Optional });
+
+				config.MaxBufferSize = this.MaxBufferSize;
+				config.MaxReceivedMessageSize = this.MaxReceivedMessageSize;
+
+				server = new HttpSelfHostServer(config);
+				server.OpenAsync();
+
+				return true;
+			}
+			catch (Exception e)
+			{
+				Logging.Write(e.ToString());
+				return false;
+			}
+		}
+
+		public bool Stop()
+		{
+			try
+			{
+				server.CloseAsync();
+				return true;
+			}
+			catch (Exception e)
+			{
+				Logging.Write(e.ToString());
+				return false;
+			}
+		}
+
+		// DO NOT DELETE
+		private void ReferencesDummy()
+		{
+			(new System.Collections.Generic.List<Type>
+			{
+				typeof(HttpPostAttribute),
+				typeof(RoutePrefixAttribute),
+				typeof(ApiController),
+				new HttpRequestMessage().Content.IsMimeMultipartContent().GetType(),
+			}).ForEach(e => Console.WriteLine(e));
+		}
+	}
+}
